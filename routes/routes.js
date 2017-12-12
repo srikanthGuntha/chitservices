74358113d3e9165a8e0ec5fabe006ca86c3d07c7
Pavan Kumar Bakku <bakkupavan@gmail.com>
2017-12-12 17:55:32 +0530

    added chitids services

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 var bodyParser = require("body-parser");
 var auth = require("./auth");
 var branches = require("../src/admin/branches");
+var chitids = require("../src/admin/chitids");
 
 module.exports = {
 
@@ -32,6 +33,11 @@ module.exports = {
         app.get("/api/v1/getbranches", branches.getbranches);
         app.put("/api/v1/updatebranches", branches.updatebranches);
         app.delete("/api/v1/deletebranches", branches.deletebranches);
+        // chitids
+        app.post("/api/v1/savechitids", chitids.savechitids);
+        app.get("/api/v1/getchitids", chitids.getchitids);
+        app.put("/api/v1/updatechitids", chitids.updatechitids);
+        app.delete("/api/v1/deletechitids", chitids.deletechitids);
 
         // dummy services
         app.get("/", function(req, res) {
---
+++ src/admin/chitids.js
@@ -0,0 +1,70 @@
+var db = require("../../config/dbconfig").db;
+var utils = require("../../utils/response");
+var mongo = require('mongodb');
+var utils = require("../../utils/response");
+
+var chitids = {
+	getchitids: function(req, res) {
+		try {
+			var userid = req.sessionuid;
+			db.chitids.find({userid: userid}).toArray(function (err, result) {
+				if(err) res.json(utils.response("failure", {"errmsg": err}));
+				res.json(utils.response("success", result));
+			});
+		} catch(err) {
+			res.json(utils.response("failure", {"errmsg": err}));
+		}
+	},
+	savechitids: function(req, res){
+		try {
+	      var chitdata = req.body;
+	      if(!chitdata){
+	        res.json(utils.response("failure", {"errmsg": "Something wrong with input data!"}));
+	      } else {
+            chitdata.userid = req.sessionuid;
+            chitdata.timestamp = new Date();
+	        db.chitids.insert(chitdata, function (err, result) {
+	          if(err) res.json(utils.response("failure", {"errmsg": err}));
+
+	          res.json(utils.response("success", result));
+	        });
+	      }
+	    } catch(err) {
+	      res.json(utils.response("failure", {"errmsg": err}));
+	    }
+	},
+	updatechitids: function(req, res){
+		try {
+            var chitdata = req.body;
+			var chitid = chitdata.chitid;
+			var chit_id = new mongo.ObjectID(chitid);
+			var userid = req.sessionuid;
+			var gquery = {"_id": chit_id, "userid": userid};
+
+			delete chitdata.chitid;
+			db.chitids.update(gquery, {$set: chitdata}, function(err, result){
+				if(err) res.json(utils.response("failure", {"errmsg": err}));
+				res.json(utils.response("success", result));
+			});
+		} catch (err) {
+			res.json(utils.response("failure", {"errmsg": err}));
+		}
+	},
+	deletechitids: function(req, res) {
+		try{
+			var chitid = req.body.chitid;
+			var chit_id = new mongo.ObjectID(chitid);
+			var userid = req.sessionuid;
+			var gquery = { "_id": chit_id, "userid": userid};
+
+			db.chitids.remove(gquery, function(err, obj) {
+				if (err) throw err;
+				res.json(utils.response("success"));
+			});
+		} catch (err) {
+			res.json(utils.response("failure", {"errmsg": err && err.message}));
+		}
+	}
+}
+
+module.exports = chitids;
